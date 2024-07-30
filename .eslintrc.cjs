5dfac1611975cdc6b3857843c420fcb69a44e65b
shohag0007 <merinedrive007@gmail.com>
2024-07-30 19:34:27 +0600

    firebase authentication Ok

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,9 +13,12 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react/jsx-no-target-blank': 'off',
+   
     'react-refresh/only-export-components': [
       'warn',
+     
       { allowConstantExport: true },
+        
     ],
   },
 }
