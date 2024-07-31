c54014bb180c09064bd6960c9721e74b7c110709
shohag0007 <merinedrive007@gmail.com>
2024-08-01 01:18:50 +0600

    shohag car doctor client side is done

--- src/pages/BookService/BookService.jsx
+++ src/pages/BookService/BookService.jsx
@@ -5,7 +5,7 @@ import { AuthContext } from "../../providers/AuthProvider";
 
 const BookService = () => {
     const service = useLoaderData();
-    const { title, _id, price } = service;
+    const {name, title, _id, price, img } = service;
     const {user} = useContext(AuthContext);
 
     const handleBookService = event => {
@@ -15,15 +15,32 @@ const BookService = () => {
         const name = form.name.value;
         const date = form.date.value;
         const email = user?.email;
-        const order = {
+        const booking = {
             customerName: name,
             email,
+            img,
             date,
-            service: _id,
+            service: title,
+            service_id: _id,
             price: price
         }
 
-        console.log(order);
+         console.log(booking);
+
+        fetch('http://localhost:5000/bookings', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(booking)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.insertedId){
+                alert('service book successfully')
+            }
+        })
     }
     return (
         <div>
--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -1,10 +1,29 @@
 import { Link } from "react-router-dom";
 import logo from '../../../assets/logo.svg';
+import { useContext } from "react";
+import { AuthContext } from "../../../providers/AuthProvider";
 
 const NavBar = () => {
+    const {user, logOut} = useContext(AuthContext);
+
+    const handleLogOut = () => {
+          logOut()
+          .then(() =>{
+
+          })
+          .catch(error => console.log(error))
+    }
     const navItems = <>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/about'>About</Link></li>
+        { user?.email ? <>
+
+            <li><Link to='/bookings'>My Bookings</Link></li>
+        
+            <li><button onClick={handleLogOut}>Logout</button></li>
+        
+        </> :<li><Link to='/login'>Login</Link></li>
+        }
         
     </>
     return (
